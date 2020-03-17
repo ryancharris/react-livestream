19061f7834a73de9d234303fad8556978527dbae
Ryan Harris <harrisryan1@gmail.com>
2020-03-16 20:54:19 -0400

    Pretty src/index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 /** @jsx jsx */
-import { useEffect, useState } from "react";
-import { css, jsx } from "@emotion/core";
-import PropTypes from "prop-types";
+import { useEffect, useState } from 'react'
+import { css, jsx } from '@emotion/core'
+import PropTypes from 'prop-types'
 
 function TwitchPlugin(props) {
-  const { apiKey, userName, aspectRatio } = props;
-  const [streamData, setStreamData] = useState(null);
+  const { apiKey, userName, aspectRatio } = props
+  const [streamData, setStreamData] = useState(null)
 
   useEffect(() => {
     fetch(`https://api.twitch.tv/helix/streams?user_login=${userName}`, {
       headers: {
-        "Client-ID": apiKey
+        'Client-ID': apiKey
       }
     })
       .then(async res => {
-        const response = await res.json();
-        setStreamData(response.data[0]);
+        const response = await res.json()
+        setStreamData(response.data[0])
       })
       .catch(err => {
-        console.log("Error fetching data from Twitch API: ", err);
-        setStreamData(null);
-      });
-  }, []);
+        console.log('Error fetching data from Twitch API: ', err)
+        setStreamData(null)
+      })
+  }, [])
 
   return streamData ? (
     <div
@@ -30,7 +30,7 @@ function TwitchPlugin(props) {
         position: relative;
 
         &::before {
-          content: "";
+          content: '';
           display: block;
           padding-bottom: calc(100% / ${aspectRatio});
         }
@@ -49,17 +49,17 @@ function TwitchPlugin(props) {
         frameBorder="0"
       ></iframe>
     </div>
-  ) : null;
+  ) : null
 }
 
-export default TwitchPlugin;
+export default TwitchPlugin
 
 TwitchPlugin.propTypes = {
   apiKey: PropTypes.string.isRequired,
   aspectRatio: PropTypes.number,
   userName: PropTypes.string.isRequired
-};
+}
 
 TwitchPlugin.defaultProps = {
   aspectRatio: 16 / 9
-};
+}
